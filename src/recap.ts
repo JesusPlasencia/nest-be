b1a67df9c543173c70f3afe7d74a00d486a9ed99
Jesus Plasencia Toledo <jesusplasencia2508@hotmail.com>
2022-09-09 10:59:44 -0500

    1C - Init with NestJS

---
+++ src/recap.ts
@@ -0,0 +1,12 @@
+class Persona {
+  constructor(private age: number, private name: string) { }
+
+  getSummary() {
+    return `My name is ${this.name}, i'm ${this.age} years old.`
+  }
+}
+
+const Jesus = new Persona(21, "Jesus Plasencia Toledo");
+const message: string = Jesus.getSummary();
+
+console.log(`INFO: ${message}`);
